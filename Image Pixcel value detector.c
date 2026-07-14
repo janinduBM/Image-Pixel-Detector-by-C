0810e2674c35cb0d9d85a9be2581c256f63524c3
BANDARA M.B.J.M.J.B.M. <135859147+janinduBM@users.noreply.github.com>
2026-07-14 14:02:23 +0530

    Add files via upload

--- Image Pixcel value detector.c
+++ Image Pixcel value detector.c
@@ -1,6 +1,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <windows.h>
 #include <stdio.h>
+
+void pixel(int topR, int topG, int topB, int bottomR, int bottomG, int bottomB);
 
 int main(){
     int width,height,channels;
@@ -25,17 +28,21 @@ int main(){
     int j=0;
     int i=0;
     for(i;i<height;){
-            j=i*width;
-            for(j;j<width*(i+1);){
-                    for(int k=0;k<3;k++){
-                            printf("%03d ",image[j+k]);
-
-                    }
-                    printf(" ");
-                    j=j+3;
+            j=i*width*3; //width is pixel width because of that we have to multipy it by 3 because one pixle has 3 value
+            for(j;j<width*(i+1)*3;){
+                    int rt,gt,bt,rb,gb,bb=0;
+                            rt=image[j];
+                            gt=image[j+1];
+                            bt=image[j+2];
+                            rb=image[j+3];
+                            gb=image[j+4];
+                            bb=image[j+5];
+                            j=j+6;
+                            pixel(rt,gt,bt,rb,gb,bb);
 
 
             }
+            printf("\n");
              i++;
 
 
@@ -44,6 +51,15 @@ int main(){
 
     return 0;
 }
+
+void pixel(int topR, int topG, int topB, int bottomR, int bottomG, int bottomB)
+{
+    printf("\x1b[38;2;%d;%d;%dm", topR, topG, topB);       // foreground (top)
+    printf("\x1b[48;2;%d;%d;%dm", bottomR, bottomG, bottomB); // background (bottom)
+    printf("▀");
+    printf("\x1b[0m"); // reset color
+}
+
 
 
 
